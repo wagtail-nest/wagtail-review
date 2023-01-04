8763abe24d7fe1589d279031a4c517f01be4d104
Matt Westcott <matt@west.co.tt>
2023-01-04 11:53:51 +0000

    Fix edge case when running wagtail-review migrations at the same time as Wagtail 4.0 migrations
    As per https://github.com/wagtail/wagtail-localize/pull/643

--- wagtail_review/migrations/0001_initial.py
+++ wagtail_review/migrations/0001_initial.py
@@ -9,19 +9,50 @@ import django.db.models.deletion
 from wagtail import VERSION as WAGTAIL_VERSION
 
 
-def get_revision_model():
+def get_run_before_and_revision_model():
+    # Keep the existing behaviour for Wagtail pre-4.0.
+    run_before = []
     revision_model = "wagtailcore.PageRevision"
     if WAGTAIL_VERSION >= (4, 0, 0):
+        # The return value of this function is used in the Migration class
+        # definition, so everything in this check happens at module load time
+        # (i.e. at the start of the `migrate` command).
+
+        # Changing the core migration dependency potentially breaks existing
+        # users as it can cause an InconsistentMigrationHistory error.
+
+        # Based on the dependencies, this migration can be run both before or
+        # after the PageRevision model is renamed to Revision. As a result,
+        # we cannot accurately determine the revision_model to use.
+
+        # What we can do instead is keep pointing to the old PageRevision name,
+        # but use run_before to make sure that this migration is run before the
+        # core migration that renames the PageRevision model.
+        run_before = [("wagtailcore", "0070_rename_pagerevision_revision")]
+
         try:
             if MigrationRecorder.Migration.objects.filter(
                 app="wagtailcore", name="0070_rename_pagerevision_revision"
             ).exists():
+                # However, if the core migration has already been applied in a
+                # previous `migrate` run, we should unset run_before to avoid an
+                # InconsistentMigrationHistory error.
+
+                # This might be the case if the core migration was run
+                # separately and an earlier version of wagtail-localize were
+                # already installed where we did not ensure this migration was
+                # run before the core migration.
+                run_before = []
+
+                # In any case, it should be safe to point to the new Revision
+                # model name as the core migration has already been applied.
                 revision_model = "wagtailcore.Revision"
+
         except (django.db.utils.OperationalError, django.db.utils.ProgrammingError):
-            # normally happens when running tests
+            # Normally happens when running tests
             pass
 
-    return revision_model
+    return run_before, revision_model
 
 
 class Migration(migrations.Migration):
@@ -34,6 +65,8 @@ class Migration(migrations.Migration):
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
+    run_before, revision_model = get_run_before_and_revision_model()
+
     operations = [
         migrations.CreateModel(
             name='Review',
@@ -41,7 +74,7 @@ class Migration(migrations.Migration):
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('status', models.CharField(choices=[('open', 'Open'), ('closed', 'Closed')], default='open', editable=False, max_length=30)),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('page_revision', models.ForeignKey(editable=False, on_delete=django.db.models.deletion.CASCADE, related_name='+', to=get_revision_model())),
+                ('page_revision', models.ForeignKey(editable=False, on_delete=django.db.models.deletion.CASCADE, related_name='+', to=revision_model)),
                 ('submitter', models.ForeignKey(editable=False, on_delete=django.db.models.deletion.CASCADE, related_name='+', to=settings.AUTH_USER_MODEL)),
             ],
             options={
